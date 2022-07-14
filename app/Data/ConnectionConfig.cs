4e21f8e03cdc8c7de3d0eb3f4362fa2ac607e30a
Parimal Raj <ingenious.raj@gmail.com>
2022-07-14 19:47:34 +0530

    server config

---
+++ app/Data/ConnectionConfig.cs
@@ -0,0 +1,6 @@
+﻿namespace TraindingApi.Data;
+
+public static class ConnectionConfig
+{
+    public static string Config = "Server=10.128.10.68;Port=5432;Database=postgres;User Id=postgres;Password=password123;CommandTimeout=20;";
+}

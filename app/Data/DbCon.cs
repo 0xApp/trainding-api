75894404cdade5d25809a7ad3478a7c6472c7354
Parimal Raj <ingenious.raj@gmail.com>
2022-07-14 19:07:42 +0530

    change db

--- app/Data/DbCon.cs
+++ app/Data/DbCon.cs
@@ -8,7 +8,7 @@ public class DbCon : IDisposable
 {
     public DbCon()
     {
-        Connection = new NpgsqlConnection("Server=10.128.10.68;Port=5432;Database=trainding;User Id=postgres;Password=password123;CommandTimeout=20;");
+        Connection = new NpgsqlConnection("Server=10.128.10.68;Port=5432;Database=postgres;User Id=postgres;Password=password123;CommandTimeout=20;");
     }
 
     private IDbConnection Connection { get; }
